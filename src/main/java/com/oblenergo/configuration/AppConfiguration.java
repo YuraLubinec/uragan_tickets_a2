1242fd02203a6c352149377ad949e97bc6b920d4
yuralubinec <yuralubinec@gmail.com>
2017-05-23 08:55:56 +0300

    added cors configuration

--- src/main/java/com/oblenergo/configuration/AppConfiguration.java
+++ src/main/java/com/oblenergo/configuration/AppConfiguration.java
@@ -1,31 +1,32 @@
 package com.oblenergo.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@Configuration
-public class AppConfiguration extends WebMvcConfigurerAdapter {
+@Configuration
+public class AppConfiguration {
 
-//  @Override
-//  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//
-//    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-//  }
-//
-//  @Override
-//  public void configureViewResolvers(ViewResolverRegistry registry) {
-//
-//    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//    viewResolver.setPrefix("/resources/static/");
-//    viewResolver.setSuffix(".html");
-//    registry.viewResolver(viewResolver);
-//  }
+  @Bean
+  public WebMvcConfigurer webMvcConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("POST", "GET", "OPTIONS", "DELETE")
+            .allowedHeaders("X-requested-with", "Content-Type").maxAge(3600);
+      }
+    };
+  }
 
-//  @Bean
-//  public MessageSource messageSource() {
-//
-//    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-//    messageSource.setBasename("validationMessage");
-//    return messageSource;
-//  }
+  // @Bean
+  // public MessageSource messageSource() {
+  //
+  // ResourceBundleMessageSource messageSource = new
+  // ResourceBundleMessageSource();
+  // messageSource.setBasename("validationMessage");
+  // return messageSource;
+  // }
 
 }
